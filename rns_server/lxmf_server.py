f73fc316e63bfa701ce7b896b460db92e1a1254b
antlas0 <150958268+antlas0@users.noreply.github.com>
2025-05-17 14:19:10 +0200

    debug info on path request

--- rns_server/lxmf_server.py
+++ rns_server/lxmf_server.py
@@ -47,6 +47,9 @@ class LXMFServer(threading.Thread):
                 RNS.Transport.request_path(self._recipient_hash)
                 while not RNS.Transport.has_path(self._recipient_hash):
                     time.sleep(0.1)
+                nh = RNS.Transport.next_hop(destination_hash=self._recipient_hash)
+                if nh: nh = nh.hex()
+                logger.info(f"Going through {nh} via {RNS.Transport.next_hop_interface(self._recipient_hash)}")
 
             recipient_identity = RNS.Identity.recall(self._recipient_hash)
             self._peer = RNS.Destination(recipient_identity, RNS.Destination.OUT, RNS.Destination.SINGLE, "lxmf", "delivery")
